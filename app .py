a2ca20fd9f124e4d1e0feaa249861778e6937020
Kafy Dier <133913272+Adhrit-Verma@users.noreply.github.com>
2023-06-25 13:54:09 +0530

    webview updates

--- app .py
+++ app .py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
 import webview
+import threading
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return render_template('index.html')
 
+def start_flask():
+    app.run(host='127.0.0.1', port=5000)
+
 if __name__ == '__main__':
-    window = webview.create_window('Maya:TS', 'http://localhost:2000')
+    t = threading.Thread(target=start_flask)
+    t.daemon = True
+    t.start()
+
+    window = webview.create_window('Maya:TS', 'http://localhost:5000',width=640,height=360)
     webview.start()
