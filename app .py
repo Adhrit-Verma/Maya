ca7068388a5033d9a63760562a7ab816d3e0deaf
Kafy Dier <133913272+Adhrit-Verma@users.noreply.github.com>
2023-06-25 13:12:08 +0530

    replaced webview wiith pyqt5

--- app .py
+++ app .py
@@ -1,9 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from cefpython3 import cefpython as cef
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    cef.Initialize()
+    cef.CreateBrowserSync(url='http://localhost:5000', window_title='Maya:TS')
+    cef.MessageLoop()
+    cef.Shutdown()
